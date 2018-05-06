3151ba60c758e6b21841891eff8cc94072c4e1c1
Leo Zheng <32986325+lzheng456@users.noreply.github.com>
2018-05-05 20:31:21 -0700

    a

--- FinalProject/src/Map.java
+++ FinalProject/src/Map.java
@@ -1,4 +1,12 @@
 
 public class Map {
+	public int width;
+	public int height;
+	public String ver;
 	
+	public Map(int width, int height, String ver) {
+		this.width = width;
+		this.height = height;
+		this.ver = ver;
+	}
 }
