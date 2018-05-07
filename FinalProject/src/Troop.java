b5d2317399ddb3be5957fbaac2a2884a9806be0f
Face733 <warrendg9@gmail.com>
2018-05-06 18:05:40 -0700

    made attack for troops

--- FinalProject/src/Troop.java
+++ FinalProject/src/Troop.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 /**
  * The superclass for all troops on the board, both enemy computer generated
  * enemies and possibly player troops if we implement them
@@ -5,30 +7,37 @@
  * @author Warren, Sepehr, Leo
  *
  */
-public abstract class Troop extends Element
-{
-   private int health;
-   private int damage;
-   
-   public Troop(int health, int damage)
-   {
-      this.health = health;
-      this.damage = damage;
-   }
+public abstract class Troop extends Element {
+	private int health, damage, speed, range;
 
-   public int health()
-   {
-      return health;
-   }
+	public Troop(int health, int damage) {
+		this.health = health;
+		this.damage = damage;
+	}
 
-   public int damage()
-   {
-      return damage;
-   }
+	public int health() {
+		return health;
+	}
 
-   public void upgrade(int health, int damage)
-   {
-      this.health += health;
-      this.damage += damage;
-   }
+	public int damage() {
+		return damage;
+	}
+
+	public void upgrade(int health, int damage) {
+		this.health += health;
+		this.damage += damage;
+	}
+
+	public Troop attack(ArrayList<Troop> troops) {
+		Troop close = null;
+		double distance = range;
+		for (Troop troop : troops) {
+			double dist = Math.pow(Math.pow(x() - troop.x(), 2) + Math.pow(y() - troop.y(), 2), 0.5);
+			if (dist <= distance) {
+				distance = dist;
+				close = troop;
+			}
+		}
+		return close;
+	}
 }
