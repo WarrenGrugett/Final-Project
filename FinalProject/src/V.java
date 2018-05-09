abbf06847abcad98d17c8a6beb65a196a0d7f010
wgrugett192 <wgrugett192@PR25.hhs.win.fuhsd.org>
2018-05-09 13:38:04 -0700

    ndksfl

--- FinalProject/src/V.java
+++ FinalProject/src/V.java
@@ -24,32 +24,13 @@ public class V {
 	// Costs of all troops
 	public static final double ARCHER_COST = 0, GIANTWARRIOR_COST = 0, KNIGHT_COST = 0, MACHINIST_COST = 0;
 	// List of all tower stats
-	// Sepehr, add the stats here, then input those into the constructors
 	public static final double[] CANNON_STATS = { 100, CANNON_COST, 3, 1 };
 	public static final double[] CHIPPER_STATS = { 10, CHIPPER_COST, 3, 5 };
 	public static final double[] TANK_STATS = { 100, TANK_COST, 3, 7 };
 	public static final double[][] TOWER_STATS = { CANNON_STATS, CHIPPER_STATS, TANK_STATS };
-	// Number of enemy units, and number of player units
+	// List of all troop stats
+	public static final double[][] TROOP_STATS = {};
 	static {
-		PApplet p = new PApplet() {
-			public void setup() {
-				CANNON_ICON = loadImage("knight.jpg");
-				CHIPPER_ICON = loadImage("knight.jpg");
-				TANK_ICON = loadImage("knight.jpg");
-				GENERATOR_ICON = loadImage("knight.jpg");
-				ARCHER_ICON = loadImage("knight.jpg");
-				GIANTWARRIOR_ICON = loadImage("knight.jpg");
-				KNIGHT_ICON = loadImage("knight.jpg");
-				MACHINIST_ICON = loadImage("knight.jpg");
-				CANNON_ATTACK_ICON = loadImage("knight.jpg");
-				CHIPPER_ATTACK_ICON = loadImage("knight.jpg");
-				TANK_ATTACK_ICON = loadImage("knight.jpg");
-				ARCHER_ATTACK_ICON = loadImage("knight.jpg");
-				GIANTWARRIOR_ATTACK_ICON = loadImage("knight.jpg");
-				KNIGHT_ATTACK_ICON = loadImage("knight.jpg");
-				MACHINIST_ATTACK_ICON = loadImage("knight.jpg");
-			}
-		};
 		P_UNITS.put(0, new Cannon(0, 0));
 		P_UNITS.put(1, new Chipper(0, 0));
 		P_UNITS.put(2, new Tank(0, 0));
@@ -63,5 +44,6 @@ public class V {
 		TROOPS.put(2, new Knight(0, 0, false));
 		TROOPS.put(3, new Machinist(0, 0, false));
 	}
+	// Number of enemy units, and number of player units
 	public static final int NUM_TROOPS = TROOPS.size(), NUM_UNITS = P_UNITS.size();
 }
