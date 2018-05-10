5e1d7a8f3ed725283edbd4a5885fe803ad4040da
Sepehr <Noushin@192.168.0.14>
2018-05-09 20:21:02 -0700

    a

--- FinalProject/src/Troop.java
+++ FinalProject/src/Troop.java
@@ -9,14 +9,16 @@ import processing.core.*;
  * @author Warren, Sepehr, Leo
  *
  */
-public abstract class Troop extends Element {
+public abstract class Troop extends Element
+{
    private PImage attackImage;
    private int health, damage, attackSpeed, delayCount;
    private float range;
    private boolean enemy;
 
    public Troop(float x, float y, int health, int damage, int attackSpeed, float range, double cost, boolean enemy,
-         PImage icon, PImage attackIcon) {
+         PImage icon, PImage attackIcon)
+   {
       super(x, y, cost, icon);
       this.health = health;
       this.damage = damage;
@@ -26,51 +28,64 @@ public abstract class Troop extends Element {
       this.attackImage = attackIcon;
    }
 
-   public void makeNextMove(Map m) {
+   public void makeNextMove(Map m)
+   {
       int[][] map = m.map();
-      float changeX = 0, changeY = 2;
+      float changeX = 0, changeY = 0;
+      int locWidth = (int) ((int) V.GRID_WIDTH / m.width());
+      int locHeight = (int) ((int) V.GRID_HEIGHT / m.height());
       move(changeX, changeY);
    }
 
-   public int health() {
+   public int health()
+   {
       return health;
    }
 
-   public int damage() {
+   public int damage()
+   {
       return damage;
    }
 
-   public int attackSpeed() {
+   public int attackSpeed()
+   {
       return attackSpeed;
    }
 
-   public boolean takeDamage(int damage) {
+   public boolean takeDamage(int damage)
+   {
       health -= damage;
       if (health < 0)
          return true;
       return false;
    }
 
-   public void upgrade(int health, int damage) {
+   public void upgrade(int health, int damage)
+   {
       this.health += health;
       this.damage += damage;
    }
 
-   public boolean attack() {
+   public boolean attack()
+   {
       delayCount++;
-      if (delayCount == attackSpeed) {
+      if (delayCount == attackSpeed)
+      {
          delayCount = 0;
          return true;
       }
       return false;
    }
 
-   public Troop attack(ArrayList<Troop> troops) {
+   public Troop attack(ArrayList<Troop> troops)
+   {
       Troop close = null;
       float distance = range;
-      for (Troop troop : troops) {
+      for (Troop troop : troops)
+      {
          float dist = (float) Math.pow(Math.pow(x() - troop.x(), 2) + Math.pow(y() - troop.y(), 2), 0.5);
-         if (dist <= distance && ((enemy && !troop.enemy) || (!enemy && troop.enemy))) {
+         if (dist <= distance && ((enemy && !troop.enemy) || (!enemy && troop.enemy)))
+         {
             distance = dist;
             close = troop;
          }
@@ -78,7 +93,8 @@ public abstract class Troop extends Element {
       return close;
    }
 
-   public void drawAttack(Troop target) {
+   public void drawAttack(Troop target)
+   {
       // Make it draw the attackImage in some way
    }
 }
