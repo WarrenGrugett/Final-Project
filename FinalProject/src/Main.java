beb32585e086582bdd4ddfc46b0315965defcd86
Face733 <warrendg9@gmail.com>
2018-05-17 21:42:30 -0700

    added a try catch because of the stupid unpreventable java error

--- FinalProject/src/Main.java
+++ FinalProject/src/Main.java
@@ -1,12 +1,16 @@
 /**
  * The class that runs the game
+ * 
  * @author Warren, Sepehr, Leo
  *
  */
 public class Main {
 
 	public static void main(String[] args) {
-		@SuppressWarnings("unused")
-		Window window = new Window();
+		try {
+			@SuppressWarnings("unused")
+			Window window = new Window();
+		} catch (Exception e) {
+		}
 	}
 }
