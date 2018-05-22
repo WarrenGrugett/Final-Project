546b2a8c86ad2de049150322893b2ba8d80852f8
Sepehr <Noushin@192.168.0.14>
2018-05-21 21:03:37 -0700

    name()

--- FinalProject/src/Archer.java
+++ FinalProject/src/Archer.java
@@ -6,27 +6,47 @@
  */
 public class Archer extends Troop
 {
+   /**
+    * Sets Archer to appropriate fields 
+    * 
+    * @param x position of Archer
+    * @param y position of Archer
+    * @param enemy if one exists
+    * @param level of the game
+    */
    public Archer(float x, float y, boolean enemy, int level)
    {
       super(x, y, (int) V.ARCHER_STATS[0], (int) V.ARCHER_STATS[1], (int) V.ARCHER_STATS[2], V.ARCHER_STATS[3],
             (int) (V.ARCHER_STATS[4] * Math.pow(1.1, level-1)), enemy, V.ARCHER_ICON, V.ARCHER_ATTACK_ICON);
    }
 
+   /**
+    * 
+    */
    public void upgrade()
    {
       super.upgrade(10, 5);
    }
-
+   
+   /**
+    * 
+    */
    public String toString()
    {
       return "Archer\nCost: " + cost();
    }
-
+   
+   /**
+    * 
+    */
    public Troop clone(float x, float y, boolean enemy, int level)
    {
       return new Archer(x, y, enemy, level);
    }
    
+   /**
+    * 
+    */
    public String name()
    {
       return "Archer";
