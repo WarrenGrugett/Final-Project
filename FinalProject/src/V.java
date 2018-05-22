59cf7a32a4e7e7fcbef172f62c5372231776db0b
Leo Zheng <32986325+lzheng456@users.noreply.github.com>
2018-05-21 22:15:25 -0700

    sound effect

--- FinalProject/src/V.java
+++ FinalProject/src/V.java
@@ -1,5 +1,5 @@
-import java.awt.*;
-import java.util.*;
+import java.awt.Point;
+import java.util.HashMap;
 
 /**
  * Class stores all variables that do not change during runtime
@@ -113,7 +113,7 @@ public class V {
 			TANK_ATTACK = new SoundEffect("explosion.wav"), GENERATOR_ATTACK = new SoundEffect(""), SNIPERTOWER_ATTACK = new SoundEffect("snipershot.wav");
 	// Troop attack sound effects
 	public static final SoundEffect ARCHER_ATTACK = new SoundEffect("arrowfired.wav"), GIANTWARRIOR_ATTACK = new SoundEffect(""),
-			KNIGHT_ATTACK = new SoundEffect(""), MACHINIST_ATTACK = new SoundEffect("");
+			KNIGHT_ATTACK = new SoundEffect(""), MACHINIST_ATTACK = new SoundEffect(""), KAMIKAZE_ATTACK = new SoundEffect("");
 	// Tower icons
 	public static final String CANNON_ICON = "cannon.png", CHIPPER_ICON = "chipper.png", TANK_ICON = "tank.png",
 			GENERATOR_ICON = "generator.png", SNIPERTOWER_ICON = "snipertower.png";
