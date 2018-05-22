78114a43ac6a36b2dbd9b3e15014693a009061b5
Face733 <warrendg9@gmail.com>
2018-05-21 21:04:56 -0700

    basically finished the map creator

--- FinalProject/src/Kamikaze.java
+++ FinalProject/src/Kamikaze.java
@@ -4,31 +4,26 @@
  * @author Sepehr
  *
  */
-public class Kamikaze extends Troop
-{
-   public Kamikaze(float x, float y, boolean enemy, int level)
-   {
-      super(x, y, (int) V.KAMIKAZE_STATS[0], (int) V.KAMIKAZE_STATS[1], (int) V.KAMIKAZE_STATS[2], V.KAMIKAZE_STATS[3],
-            (int) (V.KAMIKAZE_STATS[4] * Math.pow(1.1, level-1)), enemy, V.KAMIKAZE_ICON, V.KAMIKAZE_ATTACK_ICON);
-   }
+public class Kamikaze extends Troop {
+	public Kamikaze(float x, float y, boolean enemy, int level) {
+		super(x, y, (int) V.KAMIKAZE_STATS[0], (int) V.KAMIKAZE_STATS[1], (int) V.KAMIKAZE_STATS[2],
+				V.KAMIKAZE_STATS[3], (int) (V.KAMIKAZE_STATS[4] * Math.pow(1.1, level - 1)), enemy, V.KAMIKAZE_ICON,
+				V.KAMIKAZE_ATTACK_ICON);
+	}
 
-   public void upgrade()
-   {
-      super.upgrade(10, 5);
-   }
+	public void upgrade() {
+		super.upgrade(10, 5);
+	}
 
-   public String toString()
-   {
-      return "Kamikaze\nCost: " + cost();
-   }
+	public String toString() {
+		return "Kamikaze\nCost: " + cost();
+	}
 
-   public Troop clone(float x, float y, boolean enemy, int level)
-   {
-      return new Kamikaze(x, y, enemy, level);
-   }
-   
-   public void activateAbility()
-   {
-      
-   }
+	public Troop clone(float x, float y, boolean enemy, int level) {
+		return new Kamikaze(x, y, enemy, level);
+	}
+
+	public void activateAbility() {
+
+	}
 }
--- FinalProject/src/MapCreator.java
+++ FinalProject/src/MapCreator.java
@@ -1,20 +1,22 @@
 import java.awt.*;
-import java.util.Arrays;
-
+import java.util.*;
 import processing.core.*;
 
 public class MapCreator extends PApplet {
 	private PImage background;
 	private int[][] mapData;
-	private Point[] troops;
+	private ArrayList<Point> troops;
 	private Window w;
 	private boolean troopSelect = true;
 	private float ratio;
-	private int loc, selected = -1;
+	private int loc, selected = -1, mWidth = 15, mHeight = 15, tWidth = 64, tHeight = 64;
+	private String[] menuItems = { "Path Tile", "Wall Tile", "Enemy Start Location", "Player Start Location",
+			"Select Map Background", "Set Map Dimensions" };
 
 	public MapCreator(Window w) {
 		this.w = w;
-		troops = new Point[1000];
+		troops = new ArrayList<>();
+		mapData = new int[mHeight][mWidth];
 	}
 
 	public void run() {
@@ -25,6 +27,11 @@ public class MapCreator extends PApplet {
 	}
 
 	public void draw() {
+		if (loc <= 0)
+			loc = 1;
+		if (loc > troops.size())
+			loc = troops.size();
+		textSize(15);
 		if (!troopSelect)
 			drawMapCreator();
 		else
@@ -42,6 +49,18 @@ public class MapCreator extends PApplet {
 			image(background, 0, 0);
 		else
 			background(255);
+		textSize(30);
+		textAlign(CENTER, CENTER);
+		noFill();
+		for (int y = 0; y < mapData.length; y++) {
+			for (int x = 0; x < mapData[0].length; x++) {
+				if (mapData[y][x] != -1) {
+					text(mapData[y][x], (x + 0.5f) * tWidth, (y + 0.5f) * tHeight);
+					rect(x * tWidth, y * tWidth, tWidth, tHeight);
+				}
+			}
+		}
+		textSize(15);
 		drawMenu();
 	}
 
@@ -50,7 +69,15 @@ public class MapCreator extends PApplet {
 		scale(ratio);
 		background(255);
 		textAlign(LEFT, TOP);
-		text(Arrays.toString(troops), 5, 5);
+		textSize(30);
+		for (int i = (loc > 5) ? loc - 6 : 0; i < troops.size() && i < loc + 5; i++) {
+			if (i == loc - 1)
+				fill(255, 0, 0);
+			else
+				fill(0);
+			text(V.TROOPS.get(troops.get(i).x) + ": " + troops.get(i).y, 10, (i - loc + 6) * 80);
+		}
+		textSize(15);
 		drawMenu();
 	}
 
@@ -59,7 +86,7 @@ public class MapCreator extends PApplet {
 		float shopWidth = width / ratio - height / ratio;
 		rect(height / ratio, 0, shopWidth, height / ratio);
 		textAlign(CENTER, CENTER);
-		int num = (troopSelect) ? V.NUM_TROOPS + 2 : 7;
+		int num = (troopSelect) ? V.NUM_TROOPS + 3 : menuItems.length + 1;
 		float height = this.height / (float) num / ratio;
 		for (int i = 0; i < num - 1; i++) {
 			fill(200);
@@ -67,22 +94,14 @@ public class MapCreator extends PApplet {
 				fill(255);
 			rect(this.height / ratio, i * height, shopWidth, 0.9f * height);
 			fill(0);
-			if (troopSelect && i < num - 2)
+			if (troopSelect && i < num - 3)
 				text(V.TROOPS.get(i).toString(), width / ratio - shopWidth / 2, (i + 0.5f) * height);
+			else if (troopSelect && i == num - 2)
+				text("Delete Last Troop", width / ratio - shopWidth / 2, (i + 0.5f) * height);
 			else if (troopSelect)
 				text("No Troop", width / ratio - shopWidth / 2, (i + 0.5f) * height);
-			else if (i == 0)
-				text("Path Tile", width / ratio - shopWidth / 2, (i + 0.5f) * height);
-			else if (i == 1)
-				text("Wall Tile", width / ratio - shopWidth / 2, (i + 0.5f) * height);
-			else if (i == 2)
-				text("Enemy Start Location", width / ratio - shopWidth / 2, (i + 0.5f) * height);
-			else if (i == 3)
-				text("Player Start Location", width / ratio - shopWidth / 2, (i + 0.5f) * height);
-			else if (i == 4)
-				text("Select Level", width / ratio - shopWidth / 2, (i + 0.5f) * height);
-			else if (i == 5)
-				text("Select Map Background", width / ratio - shopWidth / 2, (i + 0.5f) * height);
+			else if (i < num - 1)
+				text(menuItems[i], width / ratio - shopWidth / 2, (i + 0.5f) * height);
 		}
 		fill(200);
 		rect(width / ratio - shopWidth, this.height / ratio - height, shopWidth, 0.9f * height);
@@ -92,34 +111,55 @@ public class MapCreator extends PApplet {
 	}
 
 	public void mousePressed() {
-		int num = (troopSelect) ? V.NUM_TROOPS + 2 : 7;
+		int num = (troopSelect) ? V.NUM_TROOPS + 3 : menuItems.length + 1;
 		float height = this.height / (float) num;
 		if (mouseX > this.height) {
 			if (mouseY % height < 0.9f * height) {
 				int y = (int) (mouseY / height);
 				if (troopSelect) {
 					if (y < V.NUM_TROOPS) {
-						if (loc > 0 && troops[loc - 1].x == y)
-							troops[loc - 1] = new Point(troops[loc - 1].x, troops[loc - 1].y + 1);
-						else
-							troops[loc] = new Point(y, 1);
-						loc++;
-					} else if (y == num - 2) {
-						troops[loc] = new Point(-1, 1);
-						loc++;
+						if (loc > 0 && troops.get(loc - 1).x == y)
+							troops.set(loc - 1, new Point(troops.get(loc - 1).x, troops.get(loc - 1).y + 1));
+						else {
+							troops.add(loc, new Point(y, 1));
+							loc++;
+						}
+					} else if (y == num - 3) {
+						if (loc > 0 && troops.get(loc - 1).x == -1)
+							troops.set(loc - 1, new Point(troops.get(loc - 1).x, troops.get(loc - 1).y + 1));
+						else {
+							troops.add(loc, new Point(-1, 1));
+							loc++;
+						}
+					} else if (y == num - 2 && loc != 0) {
+						loc--;
+						troops.remove(loc);
 					} else if (y == num - 1) {
 						troopSelect = false;
 					}
-				} else if (y == 0) {
+				} else if (y >= 0 && y < 4) {
 					if (selected != y)
 						selected = y;
 					else
 						selected = -1;
+				} else if (y == 4) {
+				} else if (y == 5) {
 				} else if (y == num - 1) {
 					troopSelect = true;
 					selected = -1;
 				}
 			}
+		} else {
+			int y = (int) (mouseY / tHeight / ratio), x = (int) (mouseX / tWidth / ratio);
+			mapData[y][x] = selected;
+		}
+	}
+
+	public void keyPressed() {
+		if (keyCode == UP) {
+			loc--;
+		} else if (keyCode == DOWN) {
+			loc++;
 		}
 	}
 }
