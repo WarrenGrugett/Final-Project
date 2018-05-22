2ec6f799a836439e547abc7a8311f5c5f77a0baf
Face733 <warrendg9@gmail.com>
2018-05-21 19:14:46 -0700

    map creator even closer to finished

--- FinalProject/src/MapCreator.java
+++ FinalProject/src/MapCreator.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.util.Arrays;
+
 import processing.core.*;
 
 public class MapCreator extends PApplet {
@@ -6,13 +8,15 @@ public class MapCreator extends PApplet {
 	private int[][] mapData;
 	private Point[] troops;
 	private Window w;
-	private boolean troopSelect;
+	private boolean troopSelect = true;
 	private float ratio;
+	private int loc, selected = -1;
 
 	public MapCreator(Window w) {
 		this.w = w;
+		troops = new Point[1000];
 	}
-	
+
 	public void run() {
 		setSize(800, 600);
 		sketchPath();
@@ -42,6 +46,11 @@ public class MapCreator extends PApplet {
 	}
 
 	private void drawTroopSelector() {
+		ratio = height / 960f;
+		scale(ratio);
+		background(255);
+		textAlign(LEFT, TOP);
+		text(Arrays.toString(troops), 5, 5);
 		drawMenu();
 	}
 
@@ -50,14 +59,18 @@ public class MapCreator extends PApplet {
 		float shopWidth = width / ratio - height / ratio;
 		rect(height / ratio, 0, shopWidth, height / ratio);
 		textAlign(CENTER, CENTER);
-		int num = (troopSelect) ? V.NUM_TROOPS + 1 : 7;
+		int num = (troopSelect) ? V.NUM_TROOPS + 2 : 7;
 		float height = this.height / (float) num / ratio;
 		for (int i = 0; i < num - 1; i++) {
 			fill(200);
+			if (selected == i)
+				fill(255);
 			rect(this.height / ratio, i * height, shopWidth, 0.9f * height);
 			fill(0);
-			if (troopSelect)
+			if (troopSelect && i < num - 2)
 				text(V.TROOPS.get(i).toString(), width / ratio - shopWidth / 2, (i + 0.5f) * height);
+			else if (troopSelect)
+				text("No Troop", width / ratio - shopWidth / 2, (i + 0.5f) * height);
 			else if (i == 0)
 				text("Path Tile", width / ratio - shopWidth / 2, (i + 0.5f) * height);
 			else if (i == 1)
@@ -71,17 +84,42 @@ public class MapCreator extends PApplet {
 			else if (i == 5)
 				text("Select Map Background", width / ratio - shopWidth / 2, (i + 0.5f) * height);
 		}
-		fill(255);
+		fill(200);
 		rect(width / ratio - shopWidth, this.height / ratio - height, shopWidth, 0.9f * height);
 		fill(0);
-		text((troopSelect) ? "Edit Map" : "Select Troops", width / ratio - shopWidth / 2, this.height / ratio - 0.5f * height - 10);
+		text((troopSelect) ? "Edit Map" : "Select Troops", width / ratio - shopWidth / 2,
+				this.height / ratio - 0.5f * height - 10);
 	}
 
 	public void mousePressed() {
-		int num = (troopSelect) ? V.NUM_TROOPS + 1 : 7;
+		int num = (troopSelect) ? V.NUM_TROOPS + 2 : 7;
 		float height = this.height / (float) num;
-		if (mouseY > this.height) {
-			
+		if (mouseX > this.height) {
+			if (mouseY % height < 0.9f * height) {
+				int y = (int) (mouseY / height);
+				if (troopSelect) {
+					if (y < V.NUM_TROOPS) {
+						if (loc > 0 && troops[loc - 1].x == y)
+							troops[loc - 1] = new Point(troops[loc - 1].x, troops[loc - 1].y + 1);
+						else
+							troops[loc] = new Point(y, 1);
+						loc++;
+					} else if (y == num - 2) {
+						troops[loc] = new Point(-1, 1);
+						loc++;
+					} else if (y == num - 1) {
+						troopSelect = false;
+					}
+				} else if (y == 0) {
+					if (selected != y)
+						selected = y;
+					else
+						selected = -1;
+				} else if (y == num - 1) {
+					troopSelect = true;
+					selected = -1;
+				}
+			}
 		}
 	}
 }
